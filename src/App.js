d16341848018d226d408a2bb292068f5c9a3f06a
Truong Phan <truong.phan2@niteco.se>
2023-01-27 14:56:44 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+  useEffect(()=> {
+    (async function(){
+      const {text} = await (await fetch('/api/message')).json();
+      setData(text);
+    })()
+  });
+
+  return <div>
+    <h1>This is my first React Appp</h1>
+    <h2>Content from Message API</h2>
+    <p>{data}</p>
+    </div>
+
 }
 
 export default App;
